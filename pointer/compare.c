41113a4034c67360d91adad1eaa2c364a0b3f0f9
roommmmmmmm <64005820@qq.com>
2016-08-25 00:02:57 +0800

    对指针的操作

---
+++ pointer/compare.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+//用来决定使用哪个函数
+#define GET_MAX 0
+#define GET_MIN 1
+
+int get_max(int i ,int j);
+
+int get_min(int i ,int j);
+
+int compare(int i ,int j, int flag);
+
+int main(){
+  int i = 5,j =10,ret;
+  ret = compare(i,j,GET_MIN);
+  printf("The Max number is %d\n",ret );
+  return 0;
+}
+
+int get_max(int i ,int j){
+  return i>j?i:j;
+}
+
+int get_min(int i ,int j) {
+  return i>j?j:i;
+}
+
+int compare(int i , int j ,int flag) {
+  int ret;
+  int (*p)(int ,int);
+  if (flag == GET_MAX) {
+    p = get_max;
+  }else{
+    p = get_min;
+  }
+  ret = p(i,j);
+  return ret;
+}
