76df02f39f4159d0a9b76d4a9e133a3a9638e0ff
roommmmmmmm <64005820@qq.com>
2016-11-07 22:11:37 +0800

    完成main函数，但是程序无输出，暂未调试成功

--- Cpp/Graph/Adjacency_List.cpp
+++ Cpp/Graph/Adjacency_List.cpp
@@ -3,7 +3,7 @@
 #define MAXN 100
 // 邻接表
 
-struct ArcNode{ // 边界点
+struct ArcNode{ // 边结点
     int adjvex; // 有向边的另一个邻接点
     ArcNode *nextarc; // 指向下一个边节点
 };
@@ -17,7 +17,7 @@ struct VNode{ // 顶点
 struct LGraph{ // 邻接表存储结构
     VNode vertexs[MAXN]; // 顶点数组
     int vexnum,arcnum; // 顶点数和边数
-}
+};
 
 LGraph lg; // 图的邻接表存储
 // 采用邻接表存储表示，构造有向图
@@ -25,22 +25,22 @@ void CreateLG() {
     int i = 0;
     ArcNode *pi;
     int v1,v2; // 有向边的两个顶点
-    for (i = 0;lg.vexnum;i++)
-        lg.vertexs[i].head1 = lg.vertexs.head2 = NULL;
+    for (i = 0;i < lg.vexnum;i++)
+        lg.vertexs[i].head1 = lg.vertexs[i].head2 = NULL;
     for(i = 0; i < lg.arcnum;i++){
         scanf("%d%d",&v1,&v2);
         v1--;v2--;
         pi = new ArcNode; // 假定有足够的空间
-        pi->ajdvex = v2;
+        pi->adjvex = v2;
         pi->nextarc = lg.vertexs[v1].head1; // 插入链表
         lg.vertexs[v1].head1 = pi;
         pi = new ArcNode;
         pi->adjvex = v1;
-        pi->nextarc = lg.vertexs[v1].head2;
+        pi->nextarc = lg.vertexs[v2].head2;
         lg.vertexs[v2].head2 = pi;
     }
 }
-
+// 释放图的邻接表各顶点的边链表中的所有边界点所占的存储空间
 void DeleteLG(){
     int i;
     ArcNode *pi;
@@ -60,4 +60,39 @@ void DeleteLG(){
             pi = lg.vertexs[i].head2;
         }
     }
+}
+int main(int argc, char const *argv[]) {
+    int i;
+    int ind,od; // 入度和出度
+    ArcNode *pi; // 遍历边链表的节点指针
+    while (1) {
+        lg.vexnum = lg.arcnum = 0;
+        scanf("%d%d", &lg.vexnum,&lg.arcnum );
+        if (lg.vexnum == 0) break;
+        CreateLG();
+        for ( i = 0; i < lg.vexnum; i++) {
+            od = 0;
+            pi = lg.vertexs[i].head1;
+            while (pi != NULL) {
+                od++;
+                pi->nextarc;
+            }
+            if (i == 0 ) printf("%d ", od);
+            else printf("%d ",od );
+        }
+        printf("\n");
+        for ( i = 0; i < lg.vexnum; i++) {
+            ind = 0;
+            pi = lg.vertexs[i].head2;
+            while (pi != NULL) {
+                ind++;
+                pi = pi->nextarc;
+            }
+            if ( i == 0) printf("%d",ind);
+            else printf("%d",ind );
+        }
+        printf("\n");
+        DeleteLG();
+    }
+    return 0;
 }
